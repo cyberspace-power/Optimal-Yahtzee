7c3ff311826cd04b17219ac271960ba86fb567e8
cyberspace-power <gjs3qd@virginia.edu>
2020-05-21 23:45:27 -0400

    First actual commit?

---
+++ Yahtzee/src/Yahtzee.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : Yahtzee.cpp
+// Author      : Gabriel Simmons, Austin Sullivan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	std::cout << "Welcome to the yahtzee program!" << std::endl; // prints Hello World!!!
+	return 0;
+}
